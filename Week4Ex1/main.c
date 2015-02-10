d1ddd2789fbb4c632f7adce7f524d815c67ddf31
laursuom <laur1.suomalainen@gmail.com>
2015-02-10 11:17:11 +0200

    Eka tehtävä

---
+++ Week4Ex1/main.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string.h>
+#define MIN(a,b) (((a)<(b))?(a):(b))
+ 
+ 
+int readLinee(int fd, char* buf, int maxlen) {
+    char c;
+    ssize_t count;
+    int i;
+    for (i = 0; i < maxlen; i++) {
+        count = read(fd, &c, 1);
+        if (count < 0) {
+            perror("read < 0\n");
+            exit(1);
+        } else if (count == 0) {
+            break;
+        }
+        if (c == '\n') {
+            break;
+        }
+        buf[i] = c;
+    }
+    return i;
+}
+ 
+/*
+ *
+ */
+int main(int argc, char** argv) {
+    int sockfd,n, len;
+    struct sockaddr_in servaddr,cliaddr;
+    char sendline[1000];
+    char recvline[1000];
+    int dataLength = atoi(argv[2]);
+    int blockLength = atoi(argv[3]);
+ 
+    if (argc != 4)
+    {
+       printf("usage:  client <IP address>\n");
+       exit(1);
+    }
+ 
+    sockfd=socket(AF_INET,SOCK_STREAM,0);
+ 
+    bzero(&servaddr,sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr=inet_addr(argv[1]);
+    servaddr.sin_port=htons(50001);
+ 
+    if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+        perror("error connecting");
+        exit(1);
+    }
+    
+    /*while ((len = readLinee(STDIN_FILENO, sendline, 1000)) > 0) 
+        write(sockfd, sendline, len);
+        write(sockfd, "\n", 1);*/
+    int i;
+    int k;
+    for (i = 0; i < dataLength; i = i + blockLength) {
+        for (k = 0; k < (MIN(blockLength, dataLength-i)); k++) {
+            sendline[k] = 'a';
+        }
+        write(sockfd, sendline, strlen(sendline));
+        memset(&sendline[0],0,sizeof(sendline));
+        sleep(1);
+    }
+    return (EXIT_SUCCESS);
+}
---
+++ Week4Ex1Server/main.c
@@ -0,0 +1,85 @@
+ 
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+ 
+int readLinee(int fd, char* buf, int maxlen) {
+    char c;
+    ssize_t count;
+    int i;
+    for (i = 0; i < maxlen; i++) {
+        count = read(fd, &c, 1);
+        if (count < 0) {
+            perror("read < 0\n");
+            exit(1);
+        } else if (count == 0) {
+            break;
+        }
+        if (c == '\n') {
+            break;
+        }
+        buf[i] = c;
+    }
+    return i;
+}
+ 
+ 
+/*
+ *
+ */
+int main(int argc, char** argv) {
+    struct sockaddr_in serv_addr, cli_addr;
+    int portno;
+    int sockfd;
+    int clilen;
+    int newsockfd;
+    int n;
+    int readLength = atoi(argv[1]);
+    char buffer[255];
+    sockfd = socket(PF_INET, SOCK_STREAM, PF_UNSPEC);
+    if (sockfd < 0) {
+        perror("Errror creating stream socket");
+        exit(1);
+    }
+    /* Initialize socket structure */
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    portno = 50001;
+   
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(portno);
+   
+   
+    /* Now bind the host address using bind() call.*/
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        perror("ERROR on binding");
+        exit(1);
+    }
+    listen(sockfd, 5);
+    while(1) {
+        clilen = sizeof(cli_addr);
+        newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+        if (newsockfd < 0){
+            perror("ERROR on accept");
+            exit(1);
+        }
+        int childpid;
+        if ( (childpid = fork()) < 0)
+            perror("Can't fork");
+        else if (childpid == 0) { /* child process */
+            close(sockfd); /* close original socket */
+            printf("waiting for the client to send text:\n");
+            while ((n = read(newsockfd, buffer, readLength)) > 0) {
+                write(STDOUT_FILENO, buffer, n);
+                write(STDOUT_FILENO, "\n", 1);
+            }
+            exit(0);
+        }
+        close(newsockfd); /* parent process */
+    }
+    return (EXIT_SUCCESS);
+}
